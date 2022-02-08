c03fbab8f23fc5ba721a2c6aee23bcaad1b4a57d
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-08 15:33:08 -0500

    Main file with display of options

--- menu.rb
+++ menu.rb
@@ -1,18 +1,16 @@
 class List
-  def initialize
-    @options = ['List of all',
-                'List all music albums',
-                'List all movies',
-                'List of games',
-                'List all genres',
-                'List all labels',
-                'List all authors',
-                'List all sources',
-                'Add a book',
-                'Add a music album',
-                'Add a movie',
-                'Add a game']
-  end
+  @options = ['List of all',
+              'List all music albums',
+              'List all movies',
+              'List of games',
+              'List all genres',
+              'List all labels',
+              'List all authors',
+              'List all sources',
+              'Add a book',
+              'Add a music album',
+              'Add a movie',
+              'Add a game']
 
   def self.list
     @options.each_with_index do |option, idx|
