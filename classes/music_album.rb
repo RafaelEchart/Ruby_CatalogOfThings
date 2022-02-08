ec1e768d4c099fb264565ac19e03b4399d25500e
codepanther <pezechigozirim@gmail.com>
2022-02-09 00:55:11 +0100

    Implement changes suggested by reviewer

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -3,9 +3,7 @@ require_relative './item'
 class MusicAlbum < Item
   def initialize(genre, publish_date, on_spotify: false)
     @on_spotify = on_spotify
-    @genre = genre
-    @publish_date = publish_date
-    super(publish_date, genre: @genre)
+    super(publish_date, genre: genre)
   end
 
   private
@@ -14,3 +12,5 @@ class MusicAlbum < Item
     @publish_date > 10 && @on_spotify
   end
 end
+
+p MusicAlbum.new('R&B', '3/2/3', on_spotify: true)
