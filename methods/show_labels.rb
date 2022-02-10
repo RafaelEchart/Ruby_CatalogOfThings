c26d633b4810d755c8064c76f8cdb84b169a6c00
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-10 12:41:13 -0500

    Fix linters errors

--- methods/show_labels.rb
+++ methods/show_labels.rb
@@ -21,58 +21,71 @@ class ShowLabel
         selected_label = gets.chomp.to_i
       end
 
-      if selected_label < @labels.length
-        books = []
-        albums = []
-        games = []
-
-        @labels[selected_label].items.each do |item|
-          instance_name_class = item.class.to_s
-          books.push(item) if instance_name_class == 'Book'
-          games.push(item) if instance_name_class == 'Game'
-          albums.push(item) if instance_name_class == 'MusicAlbum'
-        end
-
-        puts "LABEL: #{@labels[selected_label].title}, COUNT: #{@labels[selected_label].items.length} "
-        puts '-------------------------------------------------------------'
-
-        if books.length.positive?
-          puts ''
-          puts "               Books [#{books.length}]                     "
-          puts ''
-          books.each do |book|
-            puts "- Title: #{book.name}, Author: #{book.author.first_name}"
-          end
-          puts '------------------------------------------------------'
-
-        end
-
-        if albums.length.positive?
-          puts ''
-          puts "               Albums [#{albums.length}]                   "
-          puts ''
-          albums.each do |album|
-            puts "- Title: #{album.name}, Author: #{album.author.first_name}"
-          end
-          puts '------------------------------------------------------'
-        end
-
-        if games.length.positive?
-          puts ''
-          puts "               Games [#{games.length}]                      "
-          puts ''
-          games.each do |game|
-            puts "- Title: #{game.name}, Author: #{game.author.first_name}"
-          end
-          puts '------------------------------------------------------'
-
-        end
-
-        # Press any key to continue
-      end
+      organize_data(selected_label)
     else
       puts 'Empty Library, add a label first'
 
     end
+  end
+
+  def organize_data(selected_label)
+    return unless selected_label < @labels.length
+
+    books = []
+    albums = []
+    games = []
+
+    @labels[selected_label].items.each do |item|
+      instance_name_class = item.class.to_s
+      books.push(item) if instance_name_class == 'Book'
+      games.push(item) if instance_name_class == 'Game'
+      albums.push(item) if instance_name_class == 'MusicAlbum'
+    end
+
+    puts "LABEL: #{@labels[selected_label].title}, COUNT: #{@labels[selected_label].items.length} "
+    puts '-------------------------------------------------------------'
+
+    show_books(books)
+    show_albums(albums)
+    show_games(games)
+    # Press any key to continue
+  end
+
+  def show_books(books)
+    return unless books.length.positive?
+
+    puts ''
+    puts "               Books [#{books.length}]                     "
+    puts ''
+
+    books.each do |book|
+      puts "- Title: #{book.name}, Author: #{book.author.first_name}"
+    end
+    puts '------------------------------------------------------'
+  end
+
+  def show_albums(albums)
+    return unless albums.length.positive?
+
+    puts ''
+    puts "               Albums [#{albums.length}]                   "
+    puts ''
+
+    albums.each do |album|
+      puts "- Title: #{album.name}, Author: #{album.author.first_name}"
+    end
+    puts '------------------------------------------------------'
+  end
+
+  def show_games(games)
+    return unless games.length.positive?
+
+    puts ''
+    puts "               Games [#{games.length}]                      "
+    puts ''
+    games.each do |game|
+      puts "- Title: #{game.name}, Author: #{game.author.first_name}"
+    end
+    puts '------------------------------------------------------'
   end
 end
