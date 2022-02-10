709ecc122b3116fb0527e070145f9e661edcf732
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-10 16:14:59 -0500

    Save to JSON - Authors, Labels, Genres

--- memory/save_json.rb
+++ memory/save_json.rb
@@ -2,17 +2,26 @@ require 'json'
 require_relative './books_memory'
 require_relative './album_memory'
 require_relative './games_memory'
+require_relative './author_memory'
+require_relative './genre_memory'
+require_relative './label_memory'
 
 module SaveJSON
   include BooksMemory
   include AlbumsMemory
   include GamesMemory
+  include AuthorMemory
+  include GenreMemory
+  include LabelMemory
 
   def start_save
     # Check books memory is not empty
     check_books_memory
     check_albums_memory
     check_games_memory
+    check_author_memory
+    check_genre_memory
+    check_label_memory
   end
 
   def check_books_memory
@@ -44,7 +53,6 @@ module SaveJSON
     games = show_games
     return if games.empty?
 
-    p games
     array = []
     games.each do |game|
       array.push({ previous_id: game, id: game.id, name: game.name, genre: game.genre, author: game.author,
@@ -53,4 +61,39 @@ module SaveJSON
     end
     File.write('./data/games.json', JSON.dump(array))
   end
+
+  def check_author_memory
+    authors = show_authors
+    return if authors.empty?
+
+    array = []
+    authors.each do |author|
+      array.push({ previous_id: author, id: author.id, first_name: author.first_name, last_name: author.last_name,
+                   items: author.items })
+    end
+    File.write('./data/authors.json', JSON.dump(array))
+  end
+
+  def check_genre_memory
+    genres = show_genres
+    return if genres.empty?
+
+    array = []
+    genres.each do |genre|
+      array.push({ previous_id: genre, id: genre.id, name: genre.name, items: genre.items })
+    end
+    File.write('./data/genres.json', JSON.dump(array))
+  end
+  
+  def check_label_memory
+    labels = show_labels
+    return if labels.empty?
+
+    array = []
+    labels.each do |label|
+      array.push({ previous_id: label, id: label.id, title: label.title, color: label.color, items: label.items })
+    end
+    File.write('./data/labels.json', JSON.dump(array))
+  end
+
 end
