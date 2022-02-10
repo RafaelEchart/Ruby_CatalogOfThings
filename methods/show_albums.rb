f5d7fa3b81a741cd9395b25270f67c4715bf1fe4
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-10 11:37:05 -0500

    Fix Show Albums Rubocop Error

--- methods/show_albums.rb
+++ methods/show_albums.rb
@@ -7,18 +7,21 @@ class ShowAlbums
     @albums = show_albums
   end
 
+  def show_saved_albums(data)
+    data.each do |album|
+      on_spotify = album.on_spotify ? 'Yes' : 'No'
+      name = album.name
+      author = album.author.first_name
+      puts "#{data.find_index(album)}) Title: #{name}, Author: #{author}, On Spotify: #{on_spotify}"
+    end
+  end
+
   def show
     puts ''
     if @albums.length.positive?
       selected_album = @albums.length + 1
       while selected_album > @albums.length
-        @albums.each do |album|
-          on_spotify = album.on_spotify ? 'Yes' : 'No'
-          name = album.name
-          author = album.author.first_name
-          puts "#{@albums.find_index(album)}) Title: #{name}, Author: #{author}, On Spotify: #{on_spotify}"
-        end
-        puts ''
+        show_saved_albums(@albums)
         puts "#{@albums.length}) EXIT TO MENU"
         puts 'Please select a album for more information'
         selected_album = gets.chomp.to_i
