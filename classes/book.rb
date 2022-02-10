b8e17219577bdcb91976d7ad59124b1c261172b8
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-02-10 12:57:44 -0500

    Fix comments

--- classes/book.rb
+++ classes/book.rb
@@ -3,12 +3,12 @@ require_relative './item'
 class Book < Item
   attr_reader :name, :publisher, :cover_state, :publish_date, :genre, :author, :label
 
-  # def initialize(name, publisher, cover_state, publish_date, genre, author, label)
-  #   super(publish_date, genre: genre, author: author, label: label)
-  #   @name = name
-  #   @publisher = publisher
-  #   @cover_state = cover_state
-  # end
+  def initialize(name, publisher, cover_state, publish_date, genre, author, label)
+    super(publish_date, genre: genre, author: author, label: label)
+    @name = name
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
   private
 
